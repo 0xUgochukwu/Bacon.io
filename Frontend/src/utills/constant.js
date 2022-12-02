844439f41f9f3cd97492ad578d703a00b7803072
ayomide <folorunshoayomide46@gmail.com>
2022-12-02 23:20:22 +0100

    depost functionality

--- Frontend/src/utills/constant.js
+++ Frontend/src/utills/constant.js
@@ -1,4 +1,7 @@
 import abi from './AnnualBudget.json'
+import abii from './Token.json'
 
-export const contractABI = abi.abi;
-export const contractAddress = '0xAf59b767749aF21a855dF11eF6a72BDb397baDC5';
+export const contractABI = abi.abi
+export const TokenABI = abii.abi
+export const contractAddress = '0x712072dA6B70c3B2B875a20eBe206da9b866f890'
+export const TokenAddress = '0x98D7CDe70ad50119931a3e095f0feD94331eeE3F'
