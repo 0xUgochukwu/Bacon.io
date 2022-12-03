376716a20570e03977e18459f57c8b6d17755c57
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2022-12-04 00:45:58 +0100

    Update AnnualBudgetContext.jsx

--- Frontend/src/context/AnnualBudgetContext.jsx
+++ Frontend/src/context/AnnualBudgetContext.jsx
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from 'react'
-
 import { ethers } from 'ethers'
 import {
   contractABI,
   contractAddress,
+  payrollAddress,
   TokenABI,
   TokenAddress,
+  PayrollABI,
 } from '../utills/constant'
-import detectEthereumProvider from '@metamask/detect-provider'
+import { useDisconnect } from 'wagmi'
+import { keccak256 } from 'ethers/lib/utils.js'
+import { MerkleTree } from 'merkletreejs'
+
 
 export const AnnualBudgetContext = React.createContext()
 
 const { ethereum } = window
+const provider = new ethers.providers.Web3Provider(ethereum)
+
 
 const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum)
@@ -23,7 +29,6 @@ const getEthereumContract = () => {
   )
   return annualBudgetContract
 }
-
 const getTokenContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum)
   const signer = provider.getSigner()
@@ -31,42 +36,21 @@ const getTokenContract = () => {
   return matic
 }
 
+const getPayrollContract = () => {
+  const provider = new ethers.providers.Web3Provider(ethereum)
+  const signer = provider.getSigner()
+  const payroll = new ethers.Contract(payrollAddress, PayrollABI, signer)
+  console.log(payroll)
+
+}
+
 export const AnnualBudgetProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState('')
   const [isLoading, isSetLoading] = useState(false)
   const [contractBalance, setContractBalance] = useState(0)
+  const [budgetId, setBudgetId] = useState(localStorage.getItem('budgetId'))
   const [unlockTime, setUnclockTime] = useState('')
-
-  const matic = getTokenContract()
-
-  function h2d(s) {
-    function add(x, y) {
-      var c = 0,
-        r = []
-      var x = x.split('').map(Number)
-      var y = y.split('').map(Number)
-      while (x.length || y.length) {
-        var s = (x.pop() || 0) + (y.pop() || 0) + c
-        r.unshift(s < 10 ? s : s - 10)
-        c = s < 10 ? 0 : 1
-      }
-      if (c) r.unshift(c)
-      return r.join('')
-    }
-
-    var dec = '0'
-    s.split('').forEach(function (chr) {
-      var n = parseInt(chr, 16)
-      for (var t = 8; t; t >>= 1) {
-        dec = add(dec, dec)
-        if (n & t) dec = add(dec, '1')
-      }
-    })
-    return dec
-  }
-
-  
-
+  const [root, setRoot] = useState([])
 
   const connectWallet = async () => {
     try {
@@ -82,8 +66,11 @@ export const AnnualBudgetProvider = ({ children }) => {
 
   const disconnectWallet = async () => {
     try {
-      console.log('hi')
-      // ethereum.on('disconnect', handler: (error: ProviderRpcError) => void)
+      const disconnect = useDisconnect({
+        onError(error) {
+          console.log('Error', error)
+        },
+      })
     } catch (error) {}
   }
 
@@ -109,6 +96,8 @@ export const AnnualBudgetProvider = ({ children }) => {
   useEffect(() => {
     checkIfWalletIsConnected()
     balance()
+    console.log('hi')
+    getPayrollContract()
   }, [])
 
   // add items to the budget list
@@ -124,17 +113,27 @@ export const AnnualBudgetProvider = ({ children }) => {
         )
         isSetLoading(true)
         console.log(`hash is - ${budgetItem.hash}`)
-        budgetItem.wait()
-        
+        await budgetItem.wait()
+        isSetLoading(false)
+        window.location.href = "/programs/personal-savings-details"
         console.log(`hash is - ${budgetItem.hash}`)
-        budgetItem.wait()
-      })
-      isSetLoading(false)
-      
-     
+      }) 
     } catch (error) {
-      console.log(error)
+      alert("Something went wrong, please reload the page and try again")
     }
+  }
+
+  const balance = async () => {
+    //await matic.balanceOf(contractAddress)
+    const bal = await provider.getBalance(contractAddress)
+    console.log(bal)
+    isSetLoading(true)
+
+    let value = ethers.utils.formatEther(bal._hex)
+    console.log(value)
+    setContractBalance(value)
+    isSetLoading(false)
+    console.log(contractBalance)
   }
 
   const viewbudget = async () => {
@@ -151,73 +150,65 @@ export const AnnualBudgetProvider = ({ children }) => {
 
   const viewItem = async (id) => {
     try {
-
+      isSetLoading(true)
       if (!ethereum) return alert('Please Install metamask')
       const annualBudgetContract = getEthereumContract()
       const listIterm = await annualBudgetContract.listItem(id)
       console.log(listIterm)
     } catch (error) {
-      console.log(error)
+    } finally {
+      isSetLoading(false)
     }
   }
-  const removeItem = async (id) => {
+  const removeItem = async () => {
     try {
+      isSetLoading(true)
       if (!ethereum) return alert('Please Install metamask')
       const annualBudgetContract = getEthereumContract()
       const txnhash = await annualBudgetContract.removeItems(id)
       txnhash.wait()
       console.log(txnhash)
     } catch (error) {
-      console.log(error)
+    } finally {
+      isSetLoading(false)
     }
+  }
+  const getUnLockTime = async () => {
+    try {
+      if (!ethereum) return
+      const annualBudgetContract = getEthereumContract()
+      const res = await annualBudgetContract.unlockTime()
+      return res.toString()
+    } catch (error) {}
   }
   const mint = async (value) => {
     try {
-      if (!ethereum) return alert('Please Install metamask')
-      const annualBudgetContract = getEthereumContract()
-      const matic = getTokenContract()
-      const mint = await matic.mint(currentAccount, value)
-      isSetLoading(true)
-      mint.wait()
-      console.log(mint.hash)
       isLoading(false)
     } catch (error) {}
   }
-
- 
-  const getUnLockTime = async() =>{
-    try {
-         if (!ethereum) return 
-         const annualBudgetContract = getEthereumContract()
-         const res = await annualBudgetContract.unlockTime()
-         return res.toString()
-    } catch (error) {
-        
-    } 
-  }
-
-
-  const balance = async () => {
-    const bal = await matic.balanceOf(contractAddress)
-    isSetLoading(true)
-    let dbalance = h2d(bal._hex)
-    let value = dbalance.slice(dbalance.length - 1)
-    console.log(value)
-    setContractBalance(value)
-    isSetLoading(false)
-    console.log(contractBalance)
-  }
-
   const deposit = async (cost) => {
+    console.log('hi')
     try {
       if (!ethereum) return alert('Please Install metamask')
       const annualBudgetContract = getEthereumContract()
       const matic = getTokenContract()
-
-      const approval = await matic.approve(contractAddress, cost)
+      console.log(cost)
+      console.log(matic)
+      // minting
+      const mint = await matic.mint(currentAccount,cost)
       isSetLoading(true)
+      mint.wait()
+      console.log(mint.hash)
+
+      //approval
+      const approval = await matic.approve(
+        contractAddress,
+        ethers.utils.parseUnits(cost, 'ether')
+      )
       approval.wait()
       console.log(approval.hash)
+
+      //deposit
       const depositFund = await annualBudgetContract.depositBudgetFund(1, {
         value: ethers.utils.parseUnits(cost, 'ether'),
       })
@@ -227,6 +218,70 @@ export const AnnualBudgetProvider = ({ children }) => {
       isSetLoading(false)
     } catch (error) {
       throw new Error('insuffient funds')
+    }
+  }
+
+  const withdraw = async () => {
+    try {
+      if (!ethereum) return alert('Please Install metamask')
+      const annualBudgetContract = getEthereumContract()
+
+      const widthraw = await annualBudgetContract.withdraw()
+      isSetLoading(true)
+      widthraw.wait()
+      console.log(widthraw.hash)
+      isSetLoading(false)
+    } catch (error) {
+      alert("You can't withdraw now, wait for unlock time to reach before you withdraw")
+    }
+  }
+
+  const setPaymentDetails = async (items, amount, time) => {
+    try {
+      const payroll = getPayrollContract()
+
+      const encodeLeaf = items.map((addr) => keccak256(addr))
+      const merkleTree = new MerkleTree(encodeLeaf, keccak256, {
+        sortPairs: true,
+      })
+      const rootHash = merkleTree.getHexRoot()
+      setRoot[rootHash]
+      const details = await payroll.setPaymentDeatils(rootHash, amount, time);
+      details.wait()
+      console.log(details.hash)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const payrollDeposit = async (amount) => {
+    try {
+      const payroll = getPayrollContract()
+      const deposit =await payroll.depositSalaryFunds(1, {
+        value: ethers.utils.parseUnits(amount, 'ether'),
+      })
+      deposit.wait()
+      console.log(deposit.hash)
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+
+  const setOwner = async () => {
+
+  }
+
+  const claimSalary = async () => {
+    try {
+      const payroll = getPayrollContract()
+      const claim = await payroll.claimSalary(root)
+      claim.wait()
+      console.log(claim.hash)
+
+    } catch (error) {
+      console.log(error)
+      
     }
   }
 
@@ -245,12 +300,15 @@ export const AnnualBudgetProvider = ({ children }) => {
         viewItem,
         isLoading,
         isSetLoading,
-        mint,
+        getUnLockTime,
+        unlockTime,
         balance,
         contractBalance,
-        getUnLockTime,
-        unlockTime
-
+        mint,
+        withdraw,
+        claimSalary,
+        payrollDeposit,
+        setPaymentDetails,
       }}
     >
       {children}
